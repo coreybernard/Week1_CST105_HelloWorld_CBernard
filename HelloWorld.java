21e2dbdaf05ce14b9c94d790b70683424a49226c
DESKTOP-E9DLTJD\corey <CBernard7@my.gcu.edu>
2017-06-01 13:18:39 -0700

    Added HelloWorld.java
    created a hello world app and submited the .java file

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/** Program: Hello World
+* File: HelloWorld.java 
+* Summary: Prints a line to the console that says: Hello World! 
+* Author: Corey Bernard
+* Date: June 1, 2017
+**/
+
+
+public class HelloWorld
+{  // begin HelloWorld class 
+
+	public static void main(String[] args)
+	{  // begin main method 
+
+		//Prints out a line of string to the console. 
+		System.out.println("Hello World!");
+
+	} // End of the main method 
+
+} //End of the HelloWorld class 
